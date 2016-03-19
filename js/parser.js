122f99a28115f1946522ac4fb88034ccbd43525b
Mike Scopino <mikescopino@gmail.com>
2016-03-19 17:02:18 -0400

    Parsing the results into a list. Now ready for a real templating tool.

--- js/parser.js
+++ js/parser.js
@@ -1,19 +1,36 @@
 $(document).ready(function(){
-  $('#parse').click(function() {
-    $('input[type=file]').parse({
-    	config: {
-        before: function(file, inputElem) {
-          console.log('parse begins');
-        },
-        error: function(err, file, inputElem, reason) {
-          console.log('parse fucked up');
-        },
-      	complete: function(results) {
-          console.log('parse succeeded');
-          console.log(results);
-          $('#results-data').html(results.data);
-      	}
-      }
-    });
-  });
+  $('#parse').click(parse);
 });
+
+//////////////////////////////////////////
+// Parse
+//////////////////////////////////////////
+
+function parse() {
+  $('input[type=file]').parse({
+    config: {
+      before: function(file, inputElem) {
+        console.log('parse begins');
+      },
+      error: function(err, file, inputElem, reason) {
+        console.log('parse fucked up');
+      },
+      complete: function(results) {
+        var html = formatOutput(results);
+        $('#results-data').html(html);
+      }
+    }
+  });
+}
+
+function formatOutput(results) {
+  var d = results.data;
+  var output = '<ul>';
+
+  for (i=0; i < d.length; i++) {
+    output += '<li>' + d[i] + '</li>';
+  }
+  output += '</ul>';
+
+  return output;
+}
