4c5fa457544bf602b4d895d4e272fc0c255a6b4a
Mike Scopino <mikescopino@gmail.com>
2016-03-20 10:24:59 -0400

    Added Handlebars.js

--- js/parser.js
+++ js/parser.js
@@ -16,8 +16,8 @@ function parse() {
         console.log('parse fucked up');
       },
       complete: function(results) {
-        var html = formatOutput(results);
-        $('#results-data').html(html);
+        // console.log(results);
+        formatOutput(results);
       }
     }
   });
@@ -25,12 +25,18 @@ function parse() {
 
 function formatOutput(results) {
   var d = results.data;
-  var output = '<ul>';
-
-  for (i=0; i < d.length; i++) {
-    output += '<li>' + d[i] + '</li>';
+  var source = $("#results-template").html();
+  var template = Handlebars.compile(source);
+  var output = {rows : []};
+  for (i=1; i < d.length; i++) {
+    output['rows'][i] = {
+      data : d[i][0],
+      ref : d[i][1],
+      payee : d[i][2],
+      address : d[i][3],
+      amount : d[i][4]
+    }
   }
-  output += '</ul>';
 
-  return output;
+  $('.container').append(template(output));
 }
